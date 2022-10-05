d9c2c997b1cb1437039f3e17a723cf5a1ad307be
jaewoo byeon <jaewoo_byeon2@tmax.co.kr>
2022-10-05 13:18:31 +0900

    change

--- src-tauri/src/posts/show_posts.rs
+++ src-tauri/src/posts/show_posts.rs
@@ -2,7 +2,6 @@ use crate::models::Post;
 use crate::schema::posts::dsl::*;
 use crate::SqlitePool;
 use diesel::prelude::*;
-use diesel::{QueryDsl, RunQueryDsl};
 
 #[tauri::command]
 pub fn show_posts(filter_title: String, state: tauri::State<SqlitePool>) -> Vec<Post> {
